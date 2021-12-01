9ca14881786e3d82ccd6c2c5a00b90b39e1725a0
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2021-12-01 16:15:59 +0100

    Day 1 - challenge done =)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val windows = input.windowed(2, 1)
+        return windows.filter {
+            it[1].toInt() > it[0].toInt()
+        }.size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums = input.windowed(3, 1).map { list ->
+            list.sumOf { it.toInt() }
+        }
+        val windows = sums.windowed(2, 1)
+        return windows.filter { it[1] > it[0] }.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
